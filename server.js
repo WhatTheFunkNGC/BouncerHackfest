ce3643530401c885ed548b8c97a9508c5232aebd
Will Hoad <william.hoad@bt.com>
2015-07-08 17:00:08 +0100

    IT DOES SOMETHING!!!

--- server.js
+++ server.js
@@ -14,6 +14,14 @@ var address1 =	 'sip:+13334441039@sandbox.demo.alcatel-lucent.com';
 
 var callId;
 
+var announcedtmf = false;
+
+var callIdToUser;
+var callIdOfCaller;
+
+
+var bridgeIdOut;
+
 var opts = {
 	hostname: 'cowlinen.ddns.net',
 	port:80,
@@ -26,14 +34,16 @@ var opts = {
 http.createServer(function (req, res) {
 		receiveNotification(req,res);
 }).listen(5678);
+
+
 var getCallIdFromLocation =function(location){
 	var x = location.split("/");
 	return x[x.length-1];
 }
 
 function receiveNotification(req,res){
-	console.log(req.method, req.url);
-	var callId = getCallIdFromLocation(req.headers['location']);
+	//console.log(req.method, req.url);
+	callId = getCallIdFromLocation(req.headers['location']);
 	var callLocation = ["",userId,"calllegs",callId].join("/");
 	console.log("CALL LOCATION IS", callLocation);
 	var b =[];
@@ -44,26 +54,162 @@ function receiveNotification(req,res){
 		b = JSON.parse(b.join(""));
 		res.status = 200;
 		res.end();
-		if(req.headers['status'] = 'INITIAL'){
+		console.log('Status = %s',b['status']);
+		if(b['status'] == 'INITIAL'){
 			console.log("Calling notification");
-			answerCall(callLocation,b);
+			callIdOfCaller = callId;
+			setTimeout(collectDTMF,5000,1,1,300,5);
+
+			newCallLeg(callLocation,b);
+
+
+		} else {
+			console.log("NOT CALLING");
+			console.log('----------------------------------------------');
 		}
 		
 	})
 }
 
 
-function answerCall(location,call){
-	var obj = JSON.stringify({status:"CONNECTING"});
-	opts.path = location;//opts.rootPath + userId + "/callLegs/"+ callId ;
-	opts.method = "PUT";
-	console.log("IM HERE");
-	console.log("answer call..", location,"-opts-", opts,"-call-", call);
+// function answerCall(location,call){
+// 	var obj = JSON.stringify({status:"CONNECTING"});
+// 	opts.path = location;//opts.rootPath + userId + "/callLegs/"+ callId ;
+// 	opts.method = "PUT";
+// 	//console.log("answer call..", location,"-opts-", opts,"-call-", call);
+// 	http.request(opts, function (res){
+// 		console.log("\t STATUS", res.statusCode, res.headers);
+// 		res.on("data", function(ck){
+// 			var jsonReturn = JSON.parse(ck.toString());
+// 			console.log(jsonReturn);
+// 		});
+// 	}).end(obj);
+// 	console.log('---------*-------------*-------------+-----------');
+// };
+
+
+var source = 'sip:+13334441039@sandbox.demo.alcatel-lucent.com';
+var destination = 'sip:+13334441039@sandbox.demo.alcatel-lucent.com';
+var announcementText = 'this is a simulated call. press * to hangup and go to nuncance baring';
+
+function newCallLeg(callLocation,call){
+	console.log("callLocation = %s",callLocation);
+	//var obj = JSON.stringify({sourceAddress:source, targetAddress: destination, bridgeId: bridgeId,announcement:{text:announcementText}});
+	var obj = JSON.stringify({sourceAddress:source, targetAddress: destination, announcement:{text:announcementText}});
+	console.log(JSON.stringify(obj));
+	opts.path = "/" + userId + "/callLegs";//opts.rootPath + userId + "/callLegs/"+ callId ;
+	opts.method = "POST";
+	//console.log("answer call..", location,"-opts-", opts,"-call-", call);
+	opts.headers = {
+		"X-BT-FV-SESSION": sessionId,
+		'Content-Type':'application/json'
+	};
 	http.request(opts, function (res){
 		console.log("\t STATUS", res.statusCode, res.headers);
+		var location = res.headers['location'];
+		console.log("location = %s",location);
+        if (typeof callIdToUser === 'undefined')
+		      callIdToUser = getCallIdFromLocation(location);
+		var b = [];
 		res.on("data", function(ck){
 			var jsonReturn = JSON.parse(ck.toString());
-			console.log(jsonReturn);
+            if (typeof bridgeIdOut === 'undefined')
+			     bridgeIdOut = jsonReturn.bridgeId;
+			b.push(ck.toString());
+		});
+		res.on("end",function(){
+			var call = b.join("");
+			if(res.statusCode == 201){
+			 monitorLeg(callIdToUser);
+			}
+		});
+	}).end(obj);
+	console.log('---------*-------------*-------------+-----------');
+};
+
+function monitorLeg(leg){
+	opts.path='/' + userId+"/callLegs/"+leg;
+	opts.method="GET";
+	opts.headers = {
+		"X-BT-FV-SESSION": sessionId
+	};
+	http.request(opts, function(res){
+		var b = [];
+		res.on("data", function(ck){
+			b.push(ck.toString());
+			var jsonReturn =  JSON.parse(ck.toString());
+			console.log(jsonReturn.dtmf);
+			 if(!announcedtmf && jsonReturn.dtmf.length > 0 ){
+			// 	var selectedvalue = jsonReturn.dtmf[0].digits;
+			// 	if( selectedvalue == 1 || selectedvalue == 2 || selectedvalue == 3) {
+			// 		announce(" You have selected option"+selectedvalue+". Calling "+responseCodes[selectedvalue]);
+			// 		console.log(jsonReturn.bridgeId);
+			// 		setTimeout(outBoundCallA,1000,address20,"Dad");
+			// 	}else if(selectedvalue == 4){
+			// 		endCall();
+			// 	}else if(selectedvalue == 5){
+			// 			announce("turning on lights");
+			// 			mqttClient.publish("milight-control", "192.168.4.172 1 on");
+
+			console.log("recive dtmf");
+			announcedtmf = true;
+
+			console.log("killing leg %s", callIdOfCaller);
+			endCall(callIdOfCaller);
+
+			console.log("killing leg %s", callIdToUser);
+			endCall(callIdToUser);
+
+			}		
+		});
+		res.on("end",function(){
+			var result = b.join("");
+			console.log("\nLEG " + leg + " " +res.statusCode + " " + result);
+			if (res.statusCode === 200){
+				setTimeout(function(){
+					monitorLeg(leg)
+				},3000);
+			}
+		});
+	}).end();
+};
+
+function endCall(callToTerminate){
+	var status = "TERMINATED";
+	var obj = JSON.stringify({status:status});
+	opts.path = "/" + userId + "/callLegs/" + callToTerminate;
+	opts.method = "PUT";
+	opts.headers = {
+		"X-BT-FV-SESSION": sessionId,
+		'Content-Type':'application/json'
+	};
+	http.request(opts, function(res){
+		console.log("\t STATUS", res.statusCode, res.headers);
+	}).end(obj);
+};
+
+function collectDTMF(minDigits, maxDigits, timeout, maxInterval){
+	console.log("collect dtmf");
+	var dtmf = {name:"Ou lala", flush: "false",stopTones:"#", minDigits: minDigits, maxDigits: maxDigits, timeout: timeout, maxInterval: maxInterval};
+
+	var obj = JSON.stringify({dtmf: dtmf,announcement:{text:"testing text to prove something"}});
+	opts.path = '/' + userId+"/callLegs/"+callIdToUser;
+	opts.method="PUT";
+	opts.headers = {
+		"X-BT-FV-SESSION": sessionId,
+		'Content-Type':'application/json'
+	};
+	http.request(opts, function(res){
+		console.log("\t STATUS", res.statusCode, res.headers);
+				var b = [];
+		res.on("data", function(ck){
+			b.push(ck.toString());
+		});
+		res.on("end",function(){
+			var call = b.join("");
+			if (res.statusCode === 201){
+				monitorLeg(JSON.parse(call));
+			}
 		});
 	}).end(obj);
 };
